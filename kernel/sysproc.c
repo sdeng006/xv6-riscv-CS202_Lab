43020a032a9c58a8db6e7dea5bdc2b7b9fc46121
Janice Nguyen <63027233+janguyen86@users.noreply.github.com>
2023-10-18 11:01:20 -0700

    Update sysproc.c

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -118,7 +118,6 @@ sys_procinfo(struct pinfo *p)
   uint sz_aligned = PGROUNDUP(curproc->sz);
   page_usage = sz_aligned / PGSIZE; // Calculate the memory usage in terms of pages
 
-  // return 0 if successful else return -1
   if (!p)
   {
     return -1;
