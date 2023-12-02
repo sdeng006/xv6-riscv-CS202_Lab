9a2e725b0402cbf792787eba7b5495bf5622b8b2
Janice Nguyen <nguyen.janice86@gmail.com>
2023-12-02 15:00:17 -0800

    second half of part 2

--- user/thread.c
+++ user/thread.c
@@ -3,17 +3,17 @@ int thread_create(void*(start_routinue)(void*), void *arg){
 }
 
 int lock_init(volatile lock_t *lock){
-    lock->flag = 0; 
+    lock->locked = 0; 
     return 0
 }
 
 void lock_acquire(volatile lock_t *lock){
     while(1){
-        if(xchg(&lock->flag, 1) ==0)
+        if(xchg(&lock->locked, 1) ==0)
         break;
     }
 }
 
 void lock_release(volatile lock_t *lock){ 
-    xchg(&lock->flag,0); 
+    xchg(&lock->locked,0); 
 }
